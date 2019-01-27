b30ee7faa1b33b921217ce9ec2344d0ad60e8e7f
hd9719 <hd9719@gmail.com>
2019-01-28 01:33:33 +0530

    final touch

--- app.js
+++ app.js
@@ -11,6 +11,10 @@
 
   let history1=document.querySelectorAll('.tab');
 
+  let history_select=document.querySelector('#history');
+
+  let calculator_select=document.querySelector('#calculator');
+
   let number1=0,
            number2=0,
            number3=0;
@@ -56,10 +60,10 @@ function myfunction(id) {
  function calculate(id)
     {
     
-            if(display.value==='0')
-    {
-            return(alert("no input added"));
-    }  
+    //         if(display.value==='0')
+    // {
+    //         return(alert("no input added"));
+    // }  
 
 
 
@@ -129,79 +133,98 @@ case '%':      number1=parseFloat(display.value,10);
  
  //function to calculate result from previous function call and after printing to display           
 function result(num1,num2,id)
-   {               //console.log("hii");
+    {               //console.log("hii");
                 let expression;
               
 switch(id)
    
-   {
+    {
  
 case '+':       //store result in number3
-               number3=num1 + num2; 
+               number3=num1 + num2;
+               check_dot(number3); 
+               checknumber(number3);
                //set result to calculator display
-               display.value= number3;
+              // display.value= number3;
                break;
   
 case '-': 
                number3=num1 - num2; 
-               display.value= number3;
+               check_dot(number3);
+               checknumber(number3);
+               //display.value= number3;
                break;
 
 case '*':
                number3=num1 * num2;
-               display.value= number3;   
+               check_dot(number3);
+               checknumber(number3);
+               
+               //display.value= number3;   
                break;
 
 case '/':  
                number3=num1 / num2;
-               display.value = number3;    
+               check_dot(number3);
+               checknumber(number3);
+               //display.value = number3;    
                break;
 
 case 'power':
                 number3=Math.pow(num1,num2);
-                display.value=number3;
+                check_dot(number3);
+                checknumber(number3);
+                //display.value=number3;
                 break;
 
 case '%':       number3=(num1/100)*num2;
-                display.value=number3;
+                check_dot(number3);
+                checknumber(number3);
+                //display.value=number3;
                 break;
 
   
 
 
 
-   } 
+    } 
    //to add a transaction to localstorage call addtohistory function
      addtoHistory(num1,num2,id,number3);
   
-   }
+    }
  
 
-
-// function checknumber(leng,number3)
-// {
-// // //   let leng = 0;
-// //   leng=parseInt(number3.length,10);
-//    console.log(leng);
-//   if(leng < 10)
-//   {
-//     display.value = number3;
+//function to check result length
+//if greater than 10 than display error message
+function checknumber(numb)
+    {
+       
+      
+   
+               let leng='0';
+               console.log(numb);
+               leng=numb.toString().length;
   
-// }
+if(leng <= 10)
+    {
+               display.value = numb;
   
-//  else
+    }
+  
+else
 
-//  {
-//     return (alert("number greater than 10 digit"));
+    {
+                
+               display.value='result greater than 10 digit';
     
 
-// }
-// }
+    }
+    }
 
 
 //to remove 1 char from last on screen
 function clearscreen()
-   {
+    {
 
                 //reading no from display
                number1=display.value;
@@ -211,11 +234,11 @@ function clearscreen()
  
  //if length=1 then display value=0               
 if(token == 1)
-   {
+    {
                 display.value='0';
       
                 return 0;
-   }
+    }
 
               //to cut 1 no ferom total string and to display
                number2=number1.substring(0,token-1);
@@ -225,22 +248,22 @@ if(token == 1)
     
 
 
-   }
+    }
 
 
 //to clear total screen  
 function clearscreenAll()
-   {
+    {
 
 
  
                display.value='0';
      
-   }
+    }
 
 //function for single input calculation which takes first input and then operand
 function singleInput(id)
-   {
+    {
                
                //reading number value from display
                number2=display.value;
@@ -249,47 +272,50 @@ function singleInput(id)
 
 
 switch(id)
-   {
+    {
   
   
-case 'square':   number1=parseInt(display.value,10);
+case 'square':   number1=parseFloat(display.value,10);
                  //console.log(number1);
-                 number2=number1 * number1;
-                 //console.log(number2);
-                 display.value=number2;
+                 number3=number1 * number1;
+                 check_dot(number3);
+                 checknumber(number3);
                  id1='square';
-                break;
+                
+                  break;
     
-case 'cube':    number1=parseInt(display.value,10);
-                number2=number1 * number1 * number1;
-                //console.log(number2);
-                display.value = number2;
+case 'cube':    number1=parseFloat(display.value,10);
+                number3=number1 * number1 * number1;
+                console.log(number3);
+                check_dot(number3);
+                checknumber(number3);
+                
                 id1='cube';
                 break;
   
-case 'fact':    number1=parseInt(display.value,10);
-                number2 = factorial(number1); 
-                display.value=number2;
+case 'fact':    number1=parseFloat(display.value,10);
+                number3 = factorial(number1);
+                check_dot(number3);
+                checknumber(number3); 
+                
                 id1='fact' 
                 break;
              
-case 'root':    number1=parseInt(display.value,10);
-                number2=Math.sqrt(number1);
-                display.value=number2;
+case 'root':    number1=parseFloat(display.value,10);
+                number3=Math.sqrt(number1);
+                check_dot(number3);
+                checknumber(number3);
+                
                 id1='root';
                 break;
                         
    
    
-
-
-
-
-   }
+     }
 
                 //to add transaction to history to localstorage
-                addtoHistory1(id1,number1,number2);
-   }
+                addtoHistory1(id1,number1,number3);
+     }
 
 
 
@@ -300,19 +326,19 @@ function factorial(num)
    
    
 if(num==0)
-   {
+    {
                return 1;
-   }
+    }
       
 else
-   {
+    {
                for(let i=1;i<num;i++)
                fact= fact*i;
                console.log(fact);
                return fact;
-   }
+    }
    
-   }
+    }
 
 
  //function to add doubleinput transaction to local storage which takes 4 arguments  
@@ -344,7 +370,7 @@ if(expressions.length >= 10)
 
 //function to add singleinput transaction to local storage which takes 3 arguments 
 function addtoHistory1(id,num1,num2)
-  {  
+    {  
                let expressions;
    
                expressions=getHistory();
@@ -352,25 +378,25 @@ function addtoHistory1(id,num1,num2)
 
 //verifying that history data does not exceed 10 data               
 if(expressions.length >= 10)
-   {
+    {
                expressions.shift();
  
-   }
+    }
                 let expression= num1 + id + '='+ num2;
                 //pushing expression to expressions array
                 expressions.push(expression);
-                //to set expressions to localstorage with converting to  
+                //to set expressions to localstorage with converting object to string   
                 localStorage.setItem('expressions',JSON.stringify(expressions));
 
 
 
-   }
+    }
 
 
  
 //function to get history from localstorage which gives array of transactions
 function getHistory()
-   {  
+    {  
                let expressions;
                //to get item key=expressions from local storage
                const expressionLS=localStorage.getItem('expressions');
@@ -378,18 +404,18 @@ function getHistory()
 
 //checking local storage have some values or not if not then return new array
 if(expressionLS==null)
-     {
+    {
                expressions=[];
-     }
+    }
 else  
-     {          //to convert it to string parse it
+    {          //to convert it to object parse it
                expressions=JSON.parse(expressionLS);
-     }
+    }
  
                return expressions;
 
 
-     }
+    }
 
 
 //function to display history to display which gives history table
@@ -398,10 +424,10 @@ function showHistory()
        
                let row;
                //set calculator section display none(off)
-               document.querySelector('#calculator').style.display = 'none';
+               calculator_select.style.display = 'none';
                //set history table section block(ON)
-               document.querySelector('#history').style.display = 'block';
-               document.querySelector('#history').innerHTML = ' ';
+               history_select.style.display = 'block';
+                history_select.innerHTML = ' ';
 
 
               let expressions=getHistory();
@@ -434,7 +460,7 @@ for(let i=0;i<10;i++)
     }       
 
                //main append to division
-               document.querySelector('#history').appendChild(newElement);
+              history_select.appendChild(newElement);
         
         
 
@@ -447,13 +473,42 @@ function navigate()
    {
 
                //set history table section display null
-               document.querySelector('#history').style.display = 'none';
+               history_select.style.display = 'none';
                //set history table content null
-               document.querySelector('#history').innerHTML = ' ';
+               history_select.innerHTML = ' ';
                 //enable calculator section display block
-               document.querySelector('#calculator').style.display = 'block';
+               calculator_select.style.display = 'block';
 
 
    }
+
+   //function for round off decimal values which are greater than 10 digit but have small values
+   //also checks for values having power 
+function check_dot(nmr)
+    {
+     
+              let dot=nmr.toString();
+              //console.log(dot);
+
+if(dot.indexOf('+',0)>0)
+
+    {
+
+                  display.value='result greater than 10 digit';
+    }
+   
+else if(dot.indexOf('.',0)>0)
+    {
+        
+         
+              number3=parseFloat(dot.substring(0,10));
+        
+
+        
+        
+    }
+
+
+    }
 
  
